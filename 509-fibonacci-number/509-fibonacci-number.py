3d78ec811a029d747c6b9a7ff53822c1845445d8
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-09-28 15:26:30 +0300

    Time: 67 ms (34.58%), Space: 13.9 MB (55.48%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        p1=0
+        p2=1
+        
+        if n==0 or n==1:
+            return n
+        
+        temp=0
+        count=2
+        while n>1:
+            
+            temp=p1+p2
+            # if count==n:
+            #     return temp
+            p1=p2
+            p2=temp
+            n-=1
+        return temp
+            
+            
+            
+            
+            
+            
+            
+            
+       
