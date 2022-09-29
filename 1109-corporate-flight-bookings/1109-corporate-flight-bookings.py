6e2de5d71554a80d34e19b870d3634c3ca5a50e1
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-09-29 14:40:18 +0300

    Time: 1968 ms (31.59%), Space: 28.4 MB (10.86%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        
+        answer=[0]*n
+        
+        for booking in bookings:
+            
+            answer[booking[0]-1]+=booking[2]
+            
+            if booking[1]<n:
+                answer[booking[1]]-=booking[2]
+            
+        for i in range(1,len(answer)):
+            answer[i]+=answer[i-1]
+        return answer
