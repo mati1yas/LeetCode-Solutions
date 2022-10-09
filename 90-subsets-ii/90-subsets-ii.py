f0204f8b7f4f3b0d7247ec4d2a56820a7d2217d3
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-09 14:01:30 +0300

    Time: 1018 ms (5.25%), Space: 14 MB (93.88%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        
+        
+        self.result=[]
+        def sub(index,arr):
+            
+            if index>=len(nums):
+                if arr not in self.result:
+                    self.result.append(arr)
+                return 
+            recent =float('inf') 
+            for i in range(index,len(nums)):
+                # if recent=
+                if recent==nums[i]:
+                    
+                    continue
+                sub(i+1,arr+[nums[i]])
+                sub(i+1,arr)
+                recent=nums[i]
+                
+                
+                
+        nums.sort() 
+        sub(0,[])
+        return self.result
