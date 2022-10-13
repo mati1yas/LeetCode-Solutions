68664acd5c0ba128ac0340be8c60aac6d600bd74
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-13 21:54:11 +0300

    Time: 669 ms (6.28%), Space: 13.8 MB (67.66%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        
+        
+        self.operations=float('inf')
+        def dfs(num,op):
+            if num==1:
+                self.operations=min(self.operations,op)
+                return 
+            if num<=0:
+                return 
+            if num%2==0:
+                dfs(num//2,op+1)
+            else:
+                dfs(num-1,op+1)
+                dfs(num+1,op+1)
+            
+            
+        
+        dfs(n,0)
+        return self.operations
