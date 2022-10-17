e9b62580fee3491204d44057cc1f8f15d5f71f7f
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-17 21:52:19 +0300

    Time: 70 ms (6.33%), Space: 13.8 MB (97.06%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -8,7 +8,7 @@ class Solution:
 #         for these two cases we can only form one bst for each
         dp[0]=1  
         dp[1]=1
-        
+#          this is called catalan number 
         for i in range(2,n+1):
             
             for j in range(1,i+1):
