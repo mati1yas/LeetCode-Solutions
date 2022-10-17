4762f582f1c02e95f16e983df3828da0f93933c0
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-17 12:09:52 +0300

    Time: 322 ms (74.63%), Space: 25.2 MB (29.61%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -2,25 +2,30 @@ class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         
         self.steps={}
+        self.memo={}
         def dfs(num,op,x):
             if num==1:
-                self.steps[x]=op
-                return
+                return 0
             if num<1:
                 return 
+            if num not in self.memo:
+                if num%2==0:
+                    x=dfs(num//2,op+1,x)
+
+                else:
+                    x=dfs(num*3+1,op+1,x)
             
-            if num%2==0:
-                dfs(num//2,op+1,x)
-            
+                self.memo[num]=x+1
             else:
-                dfs(num*3+1,op+1,x)
-            
-            
+                self.steps[x]=self.memo[num]+op
+                
+            return self.memo[num]
         
         arr=[]
         for i in range(lo,hi+1):
             arr.append(i)
             dfs(i,0,i)
+            self.steps[i]=dfs(i,0,i)
         
         arr.sort(key=lambda x:self.steps[x])
         
