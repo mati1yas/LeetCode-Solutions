c0c3719315b8eec652097cb55f84837f63ff3ed6
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-17 11:50:07 +0300

    Time: 2373 ms (8.78%), Space: 16.8 MB (41.69%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,32 @@
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        
+        self.steps={}
+        def dfs(num,op,x):
+            if num==1:
+                self.steps[x]=op
+                return
+            if num<1:
+                return 
+            
+            if num%2==0:
+                dfs(num//2,op+1,x)
+            
+            else:
+                dfs(num*3+1,op+1,x)
+            
+            
+            
+            
+            
+            
+            
+        
+        arr=[]
+        for i in range(lo,hi+1):
+            arr.append(i)
+            dfs(i,0,i)
+        
+        arr.sort(key=lambda x:self.steps[x])
+        
+        return arr[k-1]
