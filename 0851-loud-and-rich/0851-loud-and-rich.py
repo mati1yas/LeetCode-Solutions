4bb2f01b48afb784f9e197615bfb1bf47c23d615
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-18 09:21:12 +0300

    Time: 555 ms (83.00%), Space: 23.6 MB (81.75%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -0,0 +1,38 @@
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        
+        
+        n=len(quiet)
+        
+        richerthan={i:[i] for i in range(n)}
+        indegree={i:0 for i in range(n)}
+        for rich in richer:
+            
+            richerthan[rich[0]].append(rich[1])
+            indegree[rich[1]]+=1
+        
+        queue=collections.deque()
+        print(richerthan)
+        for person in indegree:
+            if indegree[person]==0:
+                queue.append(person)
+        
+        self.answer=[i for i in range(n)]
+        while queue:
+            richperson=queue.popleft()
+            
+            for neighbor in richerthan[richperson]:
+                
+                if quiet[self.answer[neighbor]]>quiet[self.answer[richperson]]:
+                    self.answer[neighbor]=self.answer[richperson]
+               
+                
+                indegree[neighbor]-=1
+                if indegree[neighbor]==0:
+                    queue.append(neighbor)
+        return self.answer
+            
+            
+            
+            
+            
