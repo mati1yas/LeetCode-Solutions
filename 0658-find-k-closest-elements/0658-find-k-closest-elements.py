0353f60f06011badce3fc8150272f22de1e589c7
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-10-21 21:50:50 +0300

    Time: 1325 ms (5.02%), Space: 16.4 MB (7.92%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,28 +1,50 @@
+import heapq as h
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         
         
-       
-        left=0
-        right=len(arr)-k
-        # we will have four boundaries 
-        #  left   arr[mid]    arr[mid+k]    right
-        while left<right:
-            mid=(left+right)//2
-            
-            
-            if x<=arr[mid]:
-                right=mid
-            elif x>= arr[mid+k]:
-                left=mid+1
-#             the other case is when in between  the two middle boundaries
-            
-            elif x-arr[mid]>arr[mid+k]-x:
-                left=mid+1
-            else:
-                right=mid
+        distance=[(abs(x-val),val) for val in arr]
         
-        return arr[left:left+k]
+       
+        h.heapify(distance)
+        ans=[]
+        while  len(ans)<k:
+            
+            popped=h.heappop(distance)
+            ans.append(popped[1])
+        
+        ans.sort()
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         left=0
+#         right=len(arr)-k
+#         # we will have four boundaries 
+#         #  left   arr[mid]    arr[mid+k]    right
+#         while left<right:
+#             mid=(left+right)//2
+            
+            
+#             if x<=arr[mid]:
+#                 right=mid
+#             elif x>= arr[mid+k]:
+#                 left=mid+1
+# #             the other case is when in between  the two middle boundaries
+            
+#             elif x-arr[mid]>arr[mid+k]-x:
+#                 left=mid+1
+#             else:
+#                 right=mid
+        
+#         return arr[left:left+k]
         
         
             
