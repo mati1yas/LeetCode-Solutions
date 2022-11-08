4bfa05a56ca45328a77f335cda85c9924af42e60
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-11-08 15:18:17 +0300

    Time: 948 ms (12.46%), Space: 14.2 MB (20.50%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -3,23 +3,23 @@ class Solution:
     def nthUglyNumber(self, n: int) -> int:
         # the main reason i wanted heap for is to get minimum ugly 
         
-        uglies=[]
-        
-        generated=set()
         
         heap=[]
         h.heappush(heap,1)
+        generated=set({1})
         
-        for i in range(1691):
+        res=[]
+        for i in range(1,1691):
             
-            ugly = h.heappop(heap)
-            uglies.append(ugly)
-           
-            
+            ugly=h.heappop(heap)
+            res.append(ugly)
             for prime in [2,3,5]:
-                newUgly= prime* ugly 
+                
+                newUgly=prime*ugly
+                
                 if newUgly not in generated:
                     generated.add(newUgly)
                     h.heappush(heap,newUgly)
-        return uglies[n-1]
         
+                    
+        return res[n-1]
