7896632da3837263306bf7c3c23f0b534238dbbb
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2022-11-25 06:33:42 +0300

    Time: 1435 ms (91.09%), Space: 14.1 MB (13.58%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,50 +1,40 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
+        
         countBoardLetters=defaultdict(int)
-        #counting letters
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                
-                countBoardLetters[board[row][col]]+=1
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                countBoardLetters[board[i][j]]+=1
+        
         countWordLetters=Counter(word)
         
         for char in countWordLetters:
-            
-            if char not in countBoardLetters or countWordLetters[char]>countBoardLetters[char]:
+            if char not in countBoardLetters or countBoardLetters[char]<countWordLetters[char]:
                 return False
-                   
-        
-        
         inbound=lambda row,col: 0<=row<len(board) and 0<=col<len(board[0])
-        
-        def backtrack(curRow,curCol,pointer):
-            if not inbound(curRow,curCol) or word[pointer]!=board[curRow][curCol] or pointer>=len(word):
+        DIRS=[[1,0],[-1,0],[0,1],[0,-1]]
+        def backTrack(row,col,pointer):
+            
+            if not inbound(row,col) or board[row][col]!=word[pointer] or pointer>=len(word):
                 return False
+            
             if pointer==len(word)-1:
                 return True
             
             
             
-            
-            
-            DIRS=[[1,0],[0,1],[-1,0],[0,-1]]
-            temp=board[curRow][curCol]
-            board[curRow][curCol]="#"
+            temp=board[row][col]
+            board[row][col]="@"
             for x,y in DIRS:
-                
-                if backtrack (curRow+x,curCol+y,pointer+1) :
+                if backTrack(row+x,col+y,pointer+1):
                     return True
-            board[curRow][curCol]=temp
-            
-            
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                
-                if word[0]==board[row][col]:
-                    if backtrack(row,col,0):
+            board[row][col]=temp
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j]==word[0]:
+                    if backTrack(i,j,0):
                         return True
-        
-        
-        
-        
+                
