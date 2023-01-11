c80a6e77b23fe6663ba3c0a22998bee3b15c8a55
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-01-11 23:38:14 +0300

    Time: 722 ms (78.98%), Space: 50.8 MB (74.52%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        
+
+
+        graph=defaultdict(list)
+
+        for start,end in edges:
+            graph[start].append(end)
+            graph[end].append(start)        
+
+        visited=set()
+
+        def dfs(node,visited):
+          
+            total=0
+            for nbr in graph[node]:
+                if nbr not in visited:
+                    total+=dfs(nbr,visited.union({nbr}))
+            
+            if hasApple[node]:
+                total+=2
+            elif total!=0:
+                total+=2
+            
+            return total
+        ret= dfs(0,{0}) 
+        return ret-2 if ret>0 else ret
+
+         
+            
