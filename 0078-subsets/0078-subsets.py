8a3a77f4938824203bec423b9988b5a144635fa9
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-01-14 20:10:45 +0300

    Time: 35 ms (83.57%), Space: 14.1 MB (82.57%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -7,15 +7,14 @@ class Solution:
             
             if i>=len(nums):
                 self.ret.append(subset)
-                return subset
+                return 
             
             
         
             take=generateSubsets(subset+[nums[i]],i+1)
-            not_take=generateSubsets(subset,i+1)
+            not_take=generateSubsets(subset,i+1)           
             
             
             
-            return [take]+[not_take]
         generateSubsets([],0)
         return self.ret
