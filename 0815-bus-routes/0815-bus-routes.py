fd4c225713538fe8f1ecf5538a94fe6bd018c1d6
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-02-08 20:01:31 +0300

    Time: 433 ms (86.23%), Space: 38.6 MB (54.13%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.py
@@ -0,0 +1,52 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        
+        
+        indexLocations =defaultdict(list)
+        
+        
+        for i,route in enumerate(routes):
+            
+            for idx,r in enumerate(route):
+                indexLocations[r].append(i)
+        
+        adjacency=defaultdict(list)
+        
+       
+        
+        queue=collections.deque()
+        queue.append((source,0))  # start , steps
+        visited=set()
+        visitedStations=set()
+        visited.add(source)
+        
+        while queue:
+            
+            node,steps=queue.popleft()
+            
+            if node==target:
+                return steps
+            
+            for nbrIndex in indexLocations[node]:
+                
+                if nbrIndex in visitedStations:
+                    continue
+                
+                
+                for nbrBus in routes[nbrIndex]:               
+                
+                
+                    if nbrBus not in visited:
+                        visited.add(nbrBus)
+                        queue.append((nbrBus,steps+1))
+                visitedStations.add(nbrIndex)
+                    
+        return -1
+                    
+            
+            
+            
+            
+            
+            
+                
