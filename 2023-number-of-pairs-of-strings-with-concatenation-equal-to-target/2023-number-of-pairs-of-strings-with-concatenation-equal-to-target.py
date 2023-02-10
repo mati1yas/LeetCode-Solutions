57988b554cb31d375a57e77c7927379014206f3e
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-02-10 10:15:01 +0300

    Time: 80 ms (28.98%), Space: 14 MB (62.31%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -3,14 +3,10 @@ class Solution:
         count=0
         
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                
+            for j in range(len(nums)):                
                 
                 if i!=j and nums[i]+nums[j]==target:
-                    
-                    
-                
-                
+                   
                     count+=1
         
         return count
