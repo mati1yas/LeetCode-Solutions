3ce1da7a5a4168966ee817f30a651081378cd863
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-02-11 17:44:26 +0300

    Time: 777 ms (37.74%), Space: 15.8 MB (20.79%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -6,52 +6,54 @@ class Solution:
         
         
         
-        
-            
         queue=collections.deque()
+        
+        sea=0
         visited=set()
         land=0
+        
+        
+        # about to start bfs from the land
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 
-                
                 if grid[i][j]==1:
-                    land+=1  # count number of land cells
-                    visited.add((i,j))
+                    land+=1
                     queue.append((i,j))
-        DIRS=[[1,0],[0,1],[-1,0],[0,-1]]
+                    visited.add((i,j))
+                    
         
-
-        inbound = lambda row,col : 0<=row<len(grid) and 0<=col<len(grid[0])
-
-
-        distance=-1
-        sea=0
+                    
+                    
+        
+        
+        DIRS=[[1,0],[0,-1],[-1,0],[0,1]]
+        inbound= lambda row,col: 0<=row<len(grid) and 0<=col <len(grid[0])
+        level=0
+        
         while queue:
-
             
-
-
+            
             for _ in range(len(queue)):
-                curRow,curCol=queue.popleft()
-
+                curR,curC=queue.popleft()
+                
                 for x,y in DIRS:
-                    nx,ny=curRow+x,curCol+y
-
-                    if inbound(nx,ny ) and (nx,ny) not in visited:
-                        sea+=1  #  count number sea cells
-                        visited.add((nx,ny))
+                    nx,ny=x+curR,curC+y
+                    
+                    
+                    if inbound(nx,ny ) and (nx,ny ) not in visited:
+                        sea+=1
                         queue.append((nx,ny))
-
-            distance+=1
-        if sea<1 or land<1:
+                        visited.add((nx,ny))
+                        
+            level+=1
+            
+            
+            
+            
+        if not sea or not land:
             return -1
-        return distance
-
-        
-        
-        
+        return level-1
             
-            
-        
+                
                 
