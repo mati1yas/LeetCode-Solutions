58ca826b01fc0d4ee69fd18ba516e030d92a7b3e
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-02-12 12:58:24 +0300

    Time: 25 ms (93.29%), Space: 13.8 MB (98.74%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,20 +1,23 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         
-        digits=list(str(num))
         
-        ans=digits[:]
-        for i in range(len(digits)):
-            for j in range(i+1,len(digits)):
-                
-                digits[i],digits[j]=digits[j],digits[i]
-                
-                if digits>ans:
-                    ans=digits[:]
-                digits[i],digits[j]=digits[j],digits[i]
-                
+        s=list(str(num))
         
-        return int(''.join(ans))
+        ans=s[:]
+        
+        
+        for i in range(len(s)):
+            for j in range(i+1,len(s)):
+            
+                s[i],s[j]=s[j],s[i]
+                
+                if int("".join(s))>int("".join(ans)):
+                    ans=s[:]
                     
+                s[i],s[j]=s[j],s[i]
+                
         
+        return int("".join(ans))
+                
         
