df147a7b43c36e4bf4ce5d930f6bb252d6a4d8a8
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-03-10 12:02:56 +0300

    Time: 75 ms (71.75%), Space: 17.3 MB (73.23%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.py
+++ 0382-linked-list-random-node/0382-linked-list-random-node.py
@@ -12,26 +12,19 @@ class Solution:
         self.root=head
         
         self.elements=[]
-        self.count=0
+     
         
         cur=head
         
         while cur:
             self.elements.append(cur.val)
             cur = cur.next
-            self.count+=1
             
         
     def getRandom(self) -> int:       
        
         return random.choice(self.elements)
-        index=random.randint(0,self.count)
-        cur=self.root
-        while cur.next and index:
-            cur=cur.next
-            index-=1
         
-        return cur.val
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
