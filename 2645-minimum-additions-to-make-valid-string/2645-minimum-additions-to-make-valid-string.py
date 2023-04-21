357d1ac4a80da526363f0af2d5503f165dd29f77
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-04-21 17:04:33 +0300

    Time: 43 ms (29.87%), Space: 13.8 MB (96.76%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
@@ -3,7 +3,9 @@ class Solution:
         
         
         
-        
+#          the main idea of this approach is to count how many increasing sequence we have 
+
+#         if we get decreasing it means we have to start a new sequence 
         prev='z'
         sequence=0
         for char in word:
