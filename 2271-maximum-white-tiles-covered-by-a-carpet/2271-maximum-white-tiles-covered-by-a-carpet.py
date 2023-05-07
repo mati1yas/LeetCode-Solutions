56cb8ca9918cc297bf17fd630618e73cac9891b5
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-05-07 16:08:19 +0300

    Time: 1177 ms (47.90%), Space: 41.5 MB (10.92%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.py
@@ -19,23 +19,20 @@ class Solution:
         
         for i in range(n):
             
-            left = tiles[i][0]
+            left = tiles[i][0]    # we are always assuming that start of our window lies in at the start of a range. 
             
             right = min(ends[-1], left + carpetLen - 1)
-        
-        
+
             while j < n and ends[j] < right:
                 j+=1
-                
-            
-            
+               
 #             the two cases will be covered
 
             
-            if tiles[j][0] > right:
+            if tiles[j][0] > right: # case where end of window does not lie in the current range being considered 
                 ans = max(ans,prefix[j] - prefix[i])
                 
-            else:
+            else:  # case where end of the window lies 
                 ans = max(ans, prefix[j+1]-prefix[i]-ends[j]+right)
                 
             
