9d25b4ad5f63c7e5e42891aa84622380cf1f00a5
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-05-15 22:00:35 +0300

    Time: 3162 ms (27.22%), Space: 22.8 MB (66.46%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -0,0 +1,51 @@
+class Solution:
+    def maxScore(self, nums: List[int]) -> int:
+        
+   
+        
+        
+        paired=set()
+        
+        self.memo={}
+        def getScore(n,mask,):
+            
+            
+            
+            if n*2 == len(nums):
+                return 0
+            
+            
+            if mask in self.memo:
+                return self.memo[mask]
+            
+            m = 0
+            
+            for i in range(len(nums)):
+                for j in range(i+1,len(nums)):
+                    
+                    if (mask>>i)&1 == 1 or (mask>>j)&1==1:
+                        continue
+                        
+                        
+                    newMask=mask|(1<<i)|1<<j
+                    curVal=math.gcd(nums[i],nums[j])*(n+1)
+                    m=max(m,getScore(n+1,newMask)+curVal)
+                        
+                    
+            self.memo[mask]=m
+            return m
+                        
+            
+            
+            
+            
+        m=0
+        n=len(nums)//2
+        
+        mask  =  1<<len(nums)
+        return getScore(0,mask)
+            
+            
+            
+            
+            
