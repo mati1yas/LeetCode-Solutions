6a0cfbc636ad104c909cfe4bd7949e71e3ac9665
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-05-31 12:28:10 +0300

    Time: 60 ms (21.63%), Space: 16.4 MB (17.25%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -3,27 +3,36 @@ class Solution:
         
         
         memo= {} 
-        
-        
-        def dp(tar):
+        dp= [0 for _ in range(target+1)]
+        dp[0]=1
+        print(dp)
+#         def dp(tar):
+#             if tar==0:
+#                 return 1
             
-            
-            
-            if tar==0:
-                return 1
-            
-            ways=0
-            
-            if tar in memo:
+#             ways=0
+#             if tar in memo:
                 
-                return memo[tar]
+#                 return memo[tar]
+#             for val in nums:
+                
+#                 if tar-val>=0:
+#                     ways+=dp(tar-val)
+                    
+#             memo[tar]= ways    
+#             return ways
+        
+#         return dp(target)
+                    
+    
+        for tar in range(1,target+1):
+            
             for val in nums:
-                
+            
                 if tar-val>=0:
-                    ways+=dp(tar-val)
                     
-            memo[tar]= ways    
-            return ways
-        
-        return dp(target)
                     
+                    
+                    dp[tar]+=dp[tar-val]
+                    
+        return dp[-1]
