1eb4ae4d8e1654a55a4fd164a6369dcd5ab35666
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-06-07 16:24:21 +0300

    Time: 4630 ms (5.03%), Space: 551.5 MB (5.06%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        
+        memo={}
+        def dfs(index,state,trans):
+            
+            if index>=len(prices):
+                return 0
+            if trans==2:
+                return 0
+            
+            if (index,state,trans) in memo:
+            
+                return memo[(index,state,trans)]
+            profit=0
+            if state:
+                
+                sell=prices[index]+dfs(index+1,False,trans+1)
+                not_sell=dfs(index+1,True,trans)
+                profit=max(sell,not_sell)
+            else:
+                buy=dfs(index+1,True,trans)-prices[index]
+                not_buy=dfs(index+1,False,trans)
+                
+                profit=max(buy,not_buy)
+            memo[(index,state,trans)]=profit
+            return profit
+        
+              
+        
+        return dfs(0,False,0)
