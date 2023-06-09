b931f94faebcce97f490a8c8bdfc14e7341d625b
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-06-09 14:58:47 +0300

    Time: 1178 ms (10.29%), Space: 97.8 MB (17.12%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        
+        memo={}
+        
+        
+        def change(amount,index):
+            
+            if amount == 0:
+                
+                return 1
+            if index>=len(coins):
+                return 0
+            
+            ans=[]
+            
+            
+            if (amount,index) in memo:
+                
+                return memo[(amount,index)]
+            
+            
+            not_take= change(amount,index+1)
+            take=0
+            if amount-coins[index]>=0:
+                
+                take=change(amount-coins[index],index)
+                
+            
+            
+            
+            
+            
+                        
+            
+            memo[(amount,index)]=take+not_take
+            return memo[(amount,index)]
+                    
+                    
+        
+        return change(amount,0)
+        
+        
+                
