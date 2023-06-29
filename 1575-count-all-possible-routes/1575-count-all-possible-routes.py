98b426eea3ab04baae639a617e68501002cd0af2
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-06-29 11:48:04 +0300

    Time: 2708 ms (41.45%), Space: 22.8 MB (36.37%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        
+        
+        self.memo={}
+        mod=pow(10,9)+7
+        def dfs(city,fuel):
+            
+            if fuel == 0:
+                
+                if city == finish:
+                    return 1
+                else:
+                    return 0
+            
+            if (city ,fuel) in self.memo:
+                
+                return self.memo[(city,fuel)]
+            
+            total=0
+            
+            for i in range(len(locations)):
+                
+                
+            
+                if i == city :
+                    continue
+                    
+                leftFuel=fuel-abs(locations[city]-locations[i])
+                if leftFuel>=0 :
+                    
+                    total+=dfs(i,leftFuel)%mod
+                    
+            if city==finish:
+                total+=1
+            
+            self.memo[(city,fuel)]=total%mod
+            return total
+        
+        return dfs(start,fuel)%mod
+                    
+                
+                    
