58d92e92ec6a6a855533566bd046b1b0a0ab9744
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-07-17 17:57:40 +0300

    Time: 134 ms (86.48%), Space: 17.4 MB (21.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,26 +5,74 @@ class Solution:
         """
         
         
-        rows= set()
-        cols=set()
+#         rows= set()
+#         cols=set()
         
         
         
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
                 
-                if matrix[i][j]==0:
+#                 if matrix[i][j]==0:
                     
-                    rows.add(i)
-                    cols.add(j)
+#                     rows.add(i)
+#                     cols.add(j)
                     
     
         
         
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if i in rows or j in cols:
+#                     matrix[i][j]=0
+
+
+        first_col=False
+    
         for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in rows or j in cols:
+            
+            
+            if matrix[i][0]==0:
+                first_col=True
+            for j in range(1,len(matrix[0])):
+                
+                
+                if matrix[i][j]==0:
+                    
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                    
+        
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                
+                
+                if not matrix[i][0] or not matrix[0][j]:
                     matrix[i][j]=0
+                    
+        
+        
+        if matrix[0][0]==0:
+            for j in range(len(matrix[0])):
+                
+                matrix[0][j]=0
+                
+        
+        if first_col:
+            for i in range(len(matrix)):
+                
+                matrix[i][0]=0
+            
+        
+        
+        
+        
+        
+                
+
+
+
+    
         
         
         
