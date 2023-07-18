2adb417a4eab524396f354bf581efbba486e8b66
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-07-18 12:05:10 +0300

    Time: 724 ms (82.90%), Space: 77.4 MB (86.83%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -3,16 +3,17 @@ from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache=OrderedDict()
-        self.capacity=capacity
-        
+        self.cache = OrderedDict()
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         
         if key in self.cache:
-            self.cache.move_to_end(key)
+            self.cache.move_to_end(key)            
             return self.cache[key]
-        return -1
+        
+        return -1 
+        
         
 
     def put(self, key: int, value: int) -> None:
