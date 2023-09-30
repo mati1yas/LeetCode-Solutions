ca1eaa470a6069a010cc68dbecbd03faaee8ee1f
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-09-30 22:45:05 +0300

    Time: 1907 ms (14.29%), Space: 16.2 MB (99.86%) - LeetHub

---
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maximumSumOfHeights(self, maxHeights: List[int]) -> int:
+        
+        
+        
+        
+        
+#         [3,2,5,5,2,3]
+#              |     |
+        
+#         [2, 2, 5 ,5,2,2]
+#                     |
+            
+            
+            
+        mostBeautiful=0
+        for i in range(len(maxHeights)):
+            
+            heights=[0 for _ in range(len(maxHeights))]
+            
+            heights[i] = maxHeights[i]
+            
+            
+            # increasing side
+            
+            for j in range(i-1,-1,-1):
+                
+                
+                heights[j]=min(maxHeights[j],heights[j+1])
+            
+            # decreasing side
+            
+            for j in range(i+1,len(maxHeights)):
+                heights[j]=min(maxHeights[j],heights[j-1])
+                
+                
+                
+            mostBeautiful=max(sum(heights),mostBeautiful)
+                
+                
+        return mostBeautiful 
+                
+                
+                
+                
+            
