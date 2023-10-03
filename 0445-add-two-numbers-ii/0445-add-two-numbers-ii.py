3a263ce08c84142e44780db200144c1c7a6b5cf3
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-10-03 23:25:14 +0300

    Time: 62 ms (79.14%), Space: 16.4 MB (52.23%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        
+        def reverse(head):
+            
+            last=None
+            
+            while head:
+                tmp=head.next
+                head.next=last
+                
+                last=head
+                head=tmp
+                
+            return last
+            
+            
+            
+            
+            
+        l1=reverse(l1)
+        l2=reverse(l2)
+        carry=0
+        
+        head=None
+        while l1 or l2:
+            
+            if l1:
+                x1=l1.val
+            else:
+                x1=0
+                
+            if l2:
+                x2=l2.val
+            else:
+                x2=0
+                
+            
+            val=(carry+x1+x2)%10
+            carry=(carry+x1+x2)//10
+            
+            
+            
+            cur=ListNode(val)
+            cur.next=head
+            head=cur
+            
+            
+            if l1:
+                l1=l1.next
+            else:
+                l1=None
+                
+            if l2:
+                l2=l2.next
+            else:
+                l2=None
+                
+        if carry:
+            
+            cur=ListNode(carry)
+            cur.next=head
+            head=cur
+                
+        return head
+            
+            
+
