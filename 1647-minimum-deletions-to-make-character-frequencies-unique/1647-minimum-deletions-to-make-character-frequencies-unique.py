0376d92c5f6e613f7a60208064c155f67154b8c4
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-10-11 22:04:05 +0300

    Time: 102 ms (95.80%), Space: 17.1 MB (56.69%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -7,7 +7,6 @@ class Solution:
         visited=set()
         last=freq_list[-1]
         visited.add(freq_list[-1])
-        print(freq_list)
         cost=0
         for i in range(len(freq_list)-2,-1,-1):
 
@@ -19,23 +18,18 @@ class Solution:
                     cost+=1
 
                     last=cur-1
-                    visited.add(last)
-                    
-                    
-                    
+                      
                 elif last<cur:
-                    
-                    
-
                     if last>1:
                         cost+=  cur-(last-1)
                         last-=1
                     else:
                         cost+=cur
                         last=1
-                    visited.add(last)
             else:
-                visited.add(cur)
                 last= cur
+            visited.add(last)
+            
+            
                 
         return cost
