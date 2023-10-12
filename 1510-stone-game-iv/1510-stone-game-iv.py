608c76f69599225da6ffcfae6aef0d19a4044fb4
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-10-12 21:31:12 +0300

    Time: 723 ms (66.12%), Space: 16.9 MB (95.63%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,20 +1,16 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         
-        self.memo={0: False}
-        def pickStone(stones):
-            
-         
-            if stones in self.memo:
-                return self.memo[stones]
+        self.memo=[False for _ in range(n+1)]
+        
+        
+        for stones in range(n+1):
             
             for pick in range(1,int(math.sqrt(stones))+1):
                 
-                if not pickStone(stones-(pick*pick)):
+                if not self.memo[stones-(pick*pick)]:
                     self.memo[stones]=True
-                    return True
-                
-            self.memo[stones]=False
-            return False
+                    break
+        return self.memo[-1]
+    
         
-        return pickStone(n)
