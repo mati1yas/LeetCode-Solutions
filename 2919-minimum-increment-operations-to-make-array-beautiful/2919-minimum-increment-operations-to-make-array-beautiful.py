8530607b1b01abe3919c455c19208c0d19b1097f
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2023-11-09 14:30:05 +0300

    Time: 724 ms (79.64%), Space: 30.7 MB (86.46%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.py
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.py
@@ -1,28 +1,21 @@
 class Solution:
     def minIncrementOperations(self, nums: List[int], k: int) -> int:
         
+        ans=0
         
-        
-        memo={}
-        def dfs(index):
+        for i in range(len(nums)-2):
             
             
-            if index>len(nums)-3:
+            max_=max(nums[i],nums[i+1],nums[i+2])
+            
+            if max_<k:
                 
-                return 0
-            
-            if index in memo:
-                return memo[index]
-            
-            
-            
-            first = max(0,k-nums[index])+dfs(index+1)
-            second =  max(0,k-nums[index+1])+dfs(index+2)
-            third = max(0,k-nums[index+2])+dfs(index+3)
-            
-            
-            memo[index]= min(first,second,third)
-            return memo[index]
-        
-        
-        return dfs(0)
+                c=k-max_
+                
+                nums[i]+=c
+                nums[i+1]+=c
+                nums[i+2]+=c
+                
+                ans+=c
+        return ans
+                
